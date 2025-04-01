5152dae7daa1cdc5f3783ce589ed4acb6e8d5852
Santiago Rodriguez <santo1996.29@gmail.com>
2025-04-01 14:10:58 -0400

    changed stuff

--- app/LatestAdoptions.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import LatestAdoptions from "./LatestAdoptions";
-
-test("AdoptedPets has a list of pets", () => {
-  render(<LatestAdoptions />);
-  screen.getByRole("list");
-});
